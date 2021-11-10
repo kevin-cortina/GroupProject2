8ca7ef84dca30ecd663320ad3ee13f8ea194bb52
Archo21 <87740574+Archo21@users.noreply.github.com>
2021-11-09 17:02:31 -0800

    handlebars

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
-
+const exphbs =require('express-handlebars')
 // Import the connection object
-const sequelize = require('./config/connection');
+const app = express();
+const PORT = process.env.PORT || 3001;
 
+const sequelize = require('./config/connection');
+const hbs = exphbs.create({})
+app.engine('handlebars',hbs.engine)
+app.set('view engine', 'handlebars');
 const Users = require('./models/Users');
 const ListNames = require('./models/ListNames');
 const MovieIds = require('./models/MovieIds');
-
-const app = express();
-const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
