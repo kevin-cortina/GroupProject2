4ef96ade14469a50363c0493e935100aa99c4739
Archo21 <87740574+Archo21@users.noreply.github.com>
2021-11-13 12:21:53 -0800

    handlbars update

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const path= require("path")
 const exphbs =require('express-handlebars')
+const router = require("./controllers");
 // Import the connection object
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,6 +16,8 @@ const MovieIds = require('./models/MovieIds');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname,"public")));
+app.use(router);
 
 // Connect to the database before starting the Express.js server
 sequelize.sync().then(() => {
