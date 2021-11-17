ae343dea630d1ac5ffec3627f269417f4f05aa1e
Allout004 <spencerddoty@gmail.com>
2021-11-17 05:18:34 -0800

    changing back previous commit

--- public/assets/script/login.js
+++ public/assets/script/login.js
@@ -5,7 +5,7 @@ const loginFormHandler = async (event) => {
   const password = document.querySelector('#passWord').value.trim();
   if (username && password) {
     const response = await fetch('/api/users/login', {
-      method: 'GET',
+      method: 'POST',
       body: JSON.stringify({ username, password }),
       headers: { 'Content-Type': 'application/json' },
     });
