b709e3569223fb453fbbbfd005f5d86fd437304b
Chrisaeus <87839602+Chrisaeus@users.noreply.github.com>
2021-11-17 18:55:10 -0800

    bio again

--- public/assets/script/bio.js
+++ public/assets/script/bio.js
@@ -1,8 +1,8 @@
-const updateBio = (event) => {
+const updateBio = async (event) => {
     event.preventDefault();
 
-    const bio = document.querySelector("bio").value;
-
+    const bio = document.querySelector("#bio").value;
+    console.log(bio);
     const response = await fetch("api/users/bio", {
         method: 'PUT',
         body: JSON.stringify({
@@ -20,6 +20,12 @@ const updateBio = (event) => {
       }
 }
 
+const subBio = (event) => {
+    event.preventDefault();
+    console.log("You are here");
+    // document.querySelector("#bioForm").submit();
+    updateBio(event);
+}
 
-
+document.querySelector("#bioClose").addEventListener("click", subBio);
 document.querySelector("#bioForm").addEventListener("submit", updateBio);
